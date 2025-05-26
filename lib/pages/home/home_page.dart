545943c23a8034608b13f8279883af39ab190101
Min <wyes91622@gmail.com>
2025-05-27 00:49:28 +0800

    feat: Add NotificationButton widget

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:beauty_tracker/widgets/common/app_title_bar.dart';
 import 'package:beauty_tracker/widgets/home/expiring_soon_tile.dart';
+import 'package:beauty_tracker/widgets/home/notification_button.dart';
 import 'package:beauty_tracker/widgets/tabs/tab_page_scroll_view.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +16,7 @@ class HomePage extends StatelessWidget {
         AppTitleBar(
           title: 'Beauty Tracker',
           subtitle: 'Track your beauty products',
+          actionButton: NotificationButton(),
         ),
       ],
       sliver: SliverToBoxAdapter(
