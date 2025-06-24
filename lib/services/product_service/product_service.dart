bb4887c6ab4436db60fa4c4db3ba5721bcfe457c
Min <wyes91622@gmail.com>
2025-06-25 03:38:23 +0800

    feat: update ProductService methods to return specific product types

--- lib/services/product_service/product_service.dart
+++ lib/services/product_service/product_service.dart
@@ -1,8 +1,9 @@
 import 'package:beauty_tracker/errors/result.dart';
+import 'package:beauty_tracker/models/product.dart';
 import 'package:beauty_tracker/models/product_status.dart';
 
 abstract class ProductService {
-  Future<Result<void>> getAllProducts();
-  Future<Result<void>> getProductById(String productId);
-  Future<Result<void>> getProductByStatus(ProductStatus status);
+  Future<Result<List<Product>>> getAllProducts();
+  Future<Result<Product>> getProductById(String productId);
+  Future<Result<List<Product>>> getProductByStatus(ProductStatus status);
 }
