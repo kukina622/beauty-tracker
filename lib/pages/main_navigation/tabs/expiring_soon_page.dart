94c5acf9755f2b843f3f412d89f00e09c92d1332
Min <wyes91622@gmail.com>
2025-06-29 01:32:11 +0800

    refactor: implement ExpiringSoonPage to use HookWidget and fetch products

--- lib/pages/main_navigation/tabs/expiring_soon_page.dart
+++ lib/pages/main_navigation/tabs/expiring_soon_page.dart
@@ -1,70 +1,34 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:beauty_tracker/models/category.dart';
+import 'package:beauty_tracker/hooks/use_di.dart';
+import 'package:beauty_tracker/hooks/use_service_data.dart';
 import 'package:beauty_tracker/models/product.dart';
-import 'package:beauty_tracker/util/extensions/color.dart';
+import 'package:beauty_tracker/services/product_service/product_service.dart';
 import 'package:beauty_tracker/widgets/common/app_title_bar.dart';
 import 'package:beauty_tracker/widgets/page/page_scroll_view.dart';
 import 'package:beauty_tracker/widgets/product/product_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 @RoutePage()
-class ExpiringSoonPage extends StatelessWidget {
+class ExpiringSoonPage extends HookWidget {
   const ExpiringSoonPage({super.key});
-  List<Product> get products => [
-        Product(
-          id: '2',
-          name: 'Sunscreen',
-          brand: 'Brand B',
-          price: 19.99,
-          purchaseDate: DateTime(2023, 2, 20),
-          expiryDate: DateTime(2025, 6, 20),
-          categories: [
-            Category(
-              id: '789',
-              categoryName: 'Sunscreen',
-              categoryIcon: Icons.sunny.codePoint,
-              categoryColor: Colors.yellow.shade700.toInt(),
-            ),
-          ],
-        ),
-        Product(
-          id: '3',
-          name: 'Serum',
-          brand: 'Brand C',
-          price: 49.99,
-          purchaseDate: DateTime(2023, 3, 10),
-          expiryDate: DateTime(2025, 6, 10),
-          categories: [
-            Category(
-              id: '101',
-              categoryName: 'Serum',
-              categoryIcon: Icons.healing.codePoint,
-              categoryColor: Colors.green.shade200.toInt(),
-            ),
-            Category(
-              id: '102',
-              categoryName: 'Anti-aging',
-              categoryIcon: Icons.timer.codePoint,
-              categoryColor: Colors.orange.shade300.toInt(),
-            ),
-            Category(
-              id: '103',
-              categoryName: 'Brightening',
-              categoryIcon: Icons.light_mode.codePoint,
-              categoryColor: Colors.pink.shade200.toInt(),
-            ),
-            Category(
-              id: '104',
-              categoryName: 'Hydrating',
-              categoryIcon: Icons.water_drop.codePoint,
-              categoryColor: Colors.blue.shade200.toInt(),
-            ),
-          ],
-        ),
-      ];
 
   @override
   Widget build(BuildContext context) {
+    final productService = useDi<ProductService>();
+
+    final productsResult = useServiceData(
+      () => productService.getExpiringSoonProducts(),
+    );
+
+    final products = useMemoized(() {
+      if (productsResult.hasError || !productsResult.hasData) {
+        return <Product>[];
+      }
+
+      return Product.sortByExpiryDate(productsResult.data!);
+    }, [productsResult.data]);
+
     return PageScrollView(
       header: [
         AppTitleBar(
