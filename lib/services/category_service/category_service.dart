0c56c9a822248a0989b06224ce483d8814226487
Min <wyes91622@gmail.com>
2025-06-29 03:45:16 +0800

    feat: implement getAllCategories method in SupabaseCategoryServiceImpl

--- lib/services/category_service/category_service.dart
+++ lib/services/category_service/category_service.dart
@@ -3,5 +3,6 @@ import 'package:beauty_tracker/models/category.dart';
 import 'package:beauty_tracker/requests/category_requests/create_category_request.dart';
 
 abstract class CategoryService {
+  Future<Result<List<Category>>> getAllCategories();
   Future<Result<Category>> createNewCategory(CreateCategoryRequest category);
 }
