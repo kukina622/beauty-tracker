241d1b84cd1bad94ce61196f03b7ab2740d05201
Min <wyes91622@gmail.com>
2025-07-03 17:17:39 +0800

    feat: add UpdateProductRequest and implement updateProduct method in SupabaseProductServiceImpl

--- lib/services/product_service/product_service.dart
+++ lib/services/product_service/product_service.dart
@@ -2,6 +2,7 @@ import 'package:beauty_tracker/errors/result.dart';
 import 'package:beauty_tracker/models/product.dart';
 import 'package:beauty_tracker/models/product_status.dart';
 import 'package:beauty_tracker/requests/product_requests/create_product_request.dart';
+import 'package:beauty_tracker/requests/product_requests/update_product_request.dart';
 import 'package:beauty_tracker/requests/product_requests/update_product_status_request.dart';
 
 abstract class ProductService {
@@ -10,5 +11,6 @@ abstract class ProductService {
   Future<Result<List<Product>>> getProductByStatus(ProductStatus status);
   Future<Result<List<Product>>> getExpiringSoonProducts();
   Future<Result<Product>> createNewProduct(CreateProductRequest product);
+  Future<Result<Product>> updateProduct(String productId, UpdateProductRequest product);
   Future<Result<void>> bulkUpdateProductsStatus(List<UpdateProductStatusRequest> payloads);
 }
