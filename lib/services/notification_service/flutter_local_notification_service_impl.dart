3e9c94b206eab7fb15511f0edc99f6318a63bbf4
Min <wyes91622@gmail.com>
2025-07-14 02:53:41 +0800

    feat: add local notification service implementation and update Android/iOS configurations

---
+++ lib/services/notification_service/flutter_local_notification_service_impl.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:beauty_tracker/services/notification_service/notification_service.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class FlutterLocalNotificationServiceImpl implements NotificationService {
+  late FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin;
+
+  @override
+  Future<void> initialize() async {
+    _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+
+    final DarwinInitializationSettings initializationSettingsDarwin = DarwinInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+    );
+
+    final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsDarwin,
+    );
+
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  @override
+  Future<bool> requestPermissions() async {
+    final androidResult = await _flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.requestNotificationsPermission();
+
+    final iosResult = await _flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: false,
+          badge: false,
+          sound: false,
+        );
+    if (Platform.isAndroid) {
+      return androidResult ?? false;
+    }
+
+    if (Platform.isIOS) {
+      return iosResult ?? false;
+    }
+
+    return false;
+  }
+
+  @override
+  Future<void> showNotification({
+    required int id,
+    required String title,
+    required String body,
+    String? payload,
+  }) async {
+    await _flutterLocalNotificationsPlugin.show(id, title, body, null, payload: payload);
+  }
+}
---
+++ lib/services/notification_service/notification_service.dart
@@ -0,0 +1,12 @@
+abstract class NotificationService {
+  Future<void> initialize();
+
+  Future<bool> requestPermissions();
+
+  Future<void> showNotification({
+    required int id,
+    required String title,
+    required String body,
+    String? payload,
+  });
+}
