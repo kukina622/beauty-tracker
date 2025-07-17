d4d23de1545288128ca6112ede2511f73b197730
Min <wyes91622@gmail.com>
2025-07-18 03:56:35 +0800

    refactor: remove local storage dependency from ExpiryNotificationService

--- lib/app_initializer.dart
+++ lib/app_initializer.dart
@@ -67,7 +67,6 @@ class AppInitializer {
     di.registerSingleton<ExpiryNotificationService>(
       ExpiryNotificationService(
         backgroundService: di<BackgroundService>(),
-        localStorageService: di<LocalStorageService>(),
       ),
     );
   }
--- lib/services/expiry_notification_service/expiry_notification_service.dart
+++ lib/services/expiry_notification_service/expiry_notification_service.dart
@@ -1,42 +1,25 @@
 import 'package:beauty_tracker/services/background_service/background_service.dart';
 import 'package:beauty_tracker/services/background_service/task_key.dart';
+import 'package:beauty_tracker/services/background_service/work_policy.dart';
 import 'package:beauty_tracker/services/expiry_notification_service/expiry_notification_handler.dart';
-import 'package:beauty_tracker/services/local_storage_service/local_storage_keys.dart';
-import 'package:beauty_tracker/services/local_storage_service/local_storage_service.dart';
 
 class ExpiryNotificationService {
   ExpiryNotificationService({
     required this.backgroundService,
-    required this.localStorageService,
   });
 
   final BackgroundService backgroundService;
-  final LocalStorageService localStorageService;
 
   Future<void> initialize() async {
-    final isTaskRegistered =
-        await localStorageService.getBool(LocalStorageKeys.expiryNotificationTaskRegistered) ??
-            false;
-
-    if (!isTaskRegistered) {
-      await backgroundService.registerPeriodicTask(
-        taskKey: TaskKey.expireNotifications,
-        taskName: 'Check Expiring Products',
-        frequency: const Duration(hours: 24),
-      );
-
-      await localStorageService.setBool(LocalStorageKeys.expiryNotificationTaskRegistered, true);
-    }
+    await backgroundService.registerPeriodicTask(
+      taskKey: TaskKey.expireNotifications,
+      frequency: const Duration(minutes: 15),
+      workPolicy: WorkPolicy.replace,
+    );
   }
 
   Future<void> stop() async {
     await backgroundService.cancelTask(TaskKey.expireNotifications);
-
-    await localStorageService.setBool(LocalStorageKeys.expiryNotificationTaskRegistered, false);
-  }
-
-  Future<void> reset() async {
-    await localStorageService.setBool(LocalStorageKeys.expiryNotificationTaskRegistered, false);
   }
 
   Future<void> triggerManualCheck() async {
