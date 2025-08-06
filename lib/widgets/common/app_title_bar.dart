64697a5934df20818a2cca38d51956a8236c46f6
Min <wyes91622@gmail.com>
2025-08-06 23:19:03 +0800

    feat: update AppTitleBar to support customizable padding and margin

--- lib/widgets/common/app_title_bar.dart
+++ lib/widgets/common/app_title_bar.dart
@@ -8,52 +8,60 @@ class AppTitleBar extends StatelessWidget {
     this.subtitle,
     this.actionButton,
     this.backButtonEnabled = false,
+    this.padding,
+    this.margin,
   });
 
   final String title;
   final String? subtitle;
   final Widget? actionButton;
   final bool backButtonEnabled;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                if (backButtonEnabled) ...[
-                  GestureDetector(
-                    onTap: () => AutoRouter.of(context).pop(),
-                    child: const Icon(Icons.arrow_back_ios, size: 20),
-                  ),
-                  SizedBox(width: 15),
-                ],
-                Text(
-                  title,
-                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                        fontWeight: FontWeight.w800,
-                      ),
-                )
-              ],
-            ),
-            SizedBox(height: 4),
-            if (subtitle != null)
-              Text(
-                subtitle!,
-                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                      color: Colors.grey.shade600,
-                      fontWeight: FontWeight.w500,
+    return Container(
+      padding: padding,
+      margin: margin,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  if (backButtonEnabled) ...[
+                    GestureDetector(
+                      onTap: () => AutoRouter.of(context).pop(),
+                      child: const Icon(Icons.arrow_back_ios, size: 20),
                     ),
+                    SizedBox(width: 15),
+                  ],
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                          fontWeight: FontWeight.w800,
+                        ),
+                  )
+                ],
               ),
-          ],
-        ),
-        actionButton ?? const SizedBox.shrink(),
-      ],
+              SizedBox(height: 4),
+              if (subtitle != null)
+                Text(
+                  subtitle!,
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        color: Colors.grey.shade600,
+                        fontWeight: FontWeight.w500,
+                      ),
+                ),
+            ],
+          ),
+          actionButton ?? const SizedBox.shrink(),
+        ],
+      ),
     );
   }
 }
